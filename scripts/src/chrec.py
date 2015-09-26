5ec3f111531c7ad9f42644358885219c78b3c1d9
ponbiki <ponbiki@beryllium>
2015-09-26 13:59:46 -0400

    Added dict conversion and type update

--- scripts/src/chrec.py
+++ scripts/src/chrec.py
@@ -4,6 +4,7 @@ import json
 import pycurl
 from StringIO import StringIO
 from pprint import pprint
+from copy import deepcopy
 
 print('**********************************************')
 print('Simple A record <=> CNAME record swapping tool')
@@ -12,8 +13,9 @@ print('Enter your API key')
 key = raw_input()
 
 authhead = "X-NSONE-Key:" + key
-url = 'https://api.nsone.net/v1/zones'
-verb = "GET"
+base_url = 'https://api.nsone.net/v1/'
+validate_url = base_url + "zones"
+validate_verb = "GET"
 
 def buster(url, verb, authhead):
     buffer = StringIO()
@@ -26,9 +28,9 @@ def buster(url, verb, authhead):
     c.close()
     return buffer.getvalue()
 
-body = buster(url, verb, authhead)
+body = buster(validate_url, validate_verb, authhead)
 
-if json.loads(body)  == {'message': 'Unauthorized'}:
+if json.loads(body) == {'message': 'Unauthorized'}:
     print("Sorry, that key is not valid! Exiting")
     exit()
 
@@ -59,5 +61,18 @@ do_you = raw_input().lower()
 if do_you != "y":
     print('Exiting now!')
     exit()
+    
+old_rec_url = base_url + "zones/" + record + "/" + record + "/" + old_type
+old_rec_verb = "GET"
 
-print('Sucker')
+old_json = buster(old_rec_url, old_rec_verb, authhead)
+
+pprint(json.loads(old_json)[0])
+print ("\n\r\n\r")
+print(old_json)
+new_json = copy.deepcopy(old_json)
+new_json['type'] = new_type
+print ("\n\r\n\r")
+pprint(new_json)
+print ("\n\r\n\r")
+print(new_json)
