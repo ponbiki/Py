19f61a31e1e3f0592b28f3dc55e6245b2f30897b
ponbiki <ponbiki@cobalt>
2015-09-28 20:02:45 -0400

    added count parameter to the select n json

--- scripts/src/chrec.py
+++ scripts/src/chrec.py
@@ -80,7 +80,7 @@ for answer in new_json_dict['answers']:
     answer['answer'][0] = raw_input().lower() #sanitize and validate
 
 if new_type == "CNAME":
-    n_one = {'filter': 'select_first_n', 'config': {}}
+    n_one = {'filter': 'select_first_n', 'config': {'N': 1}}
     if not any (filter['filter'] == 'select_first_n' for filter in new_json_dict['filters']):
         new_json_dict['filters'].append(n_one)
         print("Added SELECT_FIRST_N filter for CNAME RFC compliance.\n")
