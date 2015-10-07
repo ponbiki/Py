90187bd03b154814130aa99592310ed6e5280130
ponbiki <ponbiki@beryllium>
2015-10-06 23:37:42 -0400

    Comparisons now sort and add to warn list correctly

--- scripts/src/zonediff.py
+++ scripts/src/zonediff.py
@@ -5,7 +5,6 @@ import pycurl
 import re
 from subprocess import check_output
 from StringIO import StringIO
-from operator import itemgetter
 from pprint import pprint
 
 API_URI = "https://api.nsone.net/v1/"
@@ -25,7 +24,6 @@ def curl_api(url, verb, authhead, *args):
     c.perform()
     c.close()
     return buffer.getvalue()
-
 def ns_get(fqdn):
     out = check_output(['dig', '+short', fqdn, 'NS'])
     legacy_ns = out.split()
@@ -46,30 +44,23 @@ def record_list(zone_json):
 def lookup(record, type, ns):
     out = check_output(['dig', '+nocmd', '+noall', '+answer', '+ttlid', type, '@' + ns, record])
     answers = out.split("\n")
-    results = []
+    answers_list = []
     for answer in answers:
         if len(answer) != 0:
-            component = answer.split("\t")
-            result = [
-                component[4], #answer
-                component[1], #ttl
-                component[0]  #record
-            ]
-            results.append(result)
-            sorted(results, key=itemgetter(0))
-    return results
+            answers_list.append(' '.join(answer.split("\t")))
+            answers_list.sort()
+    return answers_list
 
-def diff_rec(list_o_records):
-    old_list = []
-    new_list = []
-    diff_list = []
-    for record in list_o_records:
+def diff_rec(record_list):
+    warn = []
+    for record in record_list:
         if re.match(r"^\*", record['domain']):
             re.sub(r"\*", RANDOM_HOSTNAME, record['domain'], max=1)
-        old_list.append(lookup(record['domain'], record['type'], legacy_ns))
-        new_list.append(lookup(record['domain'], record['type'], NSONE_NS))
-    crap_list = [old_list, new_list]
-    return crap_list
+        old_answers = lookup(record['domain'], record['type'], legacy_ns)
+        new_answers = lookup(record['domain'], record['type'], NSONE_NS)
+        diff_list = filter(lambda x:x not in new_answers, old_answers)
+        warn.append(diff_list)
+    return warn
 
 print('Please enter API key:')
 api_key = raw_input() #sanitize
@@ -77,169 +68,4 @@ print('Please enter fully qualified domain name:') #sanitize
 fqdn = raw_input() #sanitize
 legacy_ns = ns_get(fqdn)
 pprint(diff_rec(record_list(curl_api(API_URI + "zones/" + fqdn, "GET", AUTH_HEAD + api_key))))
-
-
-
-####working copy
-#!/usr/bin/python
-
-import json
-import pycurl
-import re
-from subprocess import check_output
-from StringIO import StringIO
-#from operator import itemgetter
-from pprint import pprint
-
-API_URI = "https://api.nsone.net/v1/"
-NSONE_NS = "dns1.p01.nsone.net"
-RANDOM_HOSTNAME = "a41be866d9e771d2363d1bb6aa46c5e3"
-AUTH_HEAD = "X-NSONE-Key: "
-
-def curl_api(url, verb, authhead, *args):
-    buffer = StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.CUSTOMREQUEST, verb)
-    c.setopt(c.HTTPHEADER, [authhead])
-    for arg in args:
-        c.setopt(c.POSTFIELDS, arg)
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    c.close()
-    return buffer.getvalue()
-
-def ns_get(fqdn):
-    out = check_output(['dig', '+short', fqdn, 'NS'])
-    legacy_ns = out.split()
-    return legacy_ns[0]
-
-def record_list(zone_json):
-    zone = json.loads(zone_json)
-    rec_list = []
-    for records in zone['records']:
-        if records['type'] != 'NS':
-            recs = {
-                'domain': records['domain'],
-                'type': records['type']
-            }
-            rec_list.append(recs)
-    return rec_list
-
-def lookup(record, type, ns):
-    out = check_output(['dig', '+nocmd', '+noall', '+answer', '+ttlid', type, '@' + ns, record])
-    answers = out.split("\n")
-    results = []
-    answers_list = []
-    for answer in answers:
-        if len(answer) != 0:
-            component = ' '.join(answer.split("\t"))
-            answers_list.append(component)
-            sorted(answers_list)
-        #sorted(results, key=itemgetter(len(results)-1))
-        results.append(answers_list)
-    return results
-
-def diff_rec(list_o_records):
-    old_list = []
-    new_list = []
-    diff_list = []
-    for record in list_o_records:
-        if re.match(r"^\*", record['domain']):
-            re.sub(r"\*", RANDOM_HOSTNAME, record['domain'], max=1)
-        old_list.append(lookup(record['domain'], record['type'], legacy_ns))
-        new_list.append(lookup(record['domain'], record['type'], NSONE_NS))
-    diff_list = filter(lambda x:x not in new_list, old_list)
-    return diff_list
-
-print('Please enter API key:')
-api_key = raw_input() #sanitize
-print('Please enter fully qualified domain name:') #sanitize
-fqdn = raw_input() #sanitize
-legacy_ns = ns_get(fqdn)
-pprint(diff_rec(record_list(curl_api(API_URI + "zones/" + fqdn, "GET", AUTH_HEAD + api_key))))
-
-###################nano copy
-
-
-
-#!/usr/bin/python
-
-import json
-import pycurl
-import re
-from subprocess import check_output
-from StringIO import StringIO
-from operator import itemgetter
-from pprint import pprint
-
-API_URI = "https://api.nsone.net/v1/"
-NSONE_NS = "dns1.p01.nsone.net"
-RANDOM_HOSTNAME = "a41be866d9e771d2363d1bb6aa46c5e3"
-AUTH_HEAD = "X-NSONE-Key: "
-
-def curl_api(url, verb, authhead, *args):
-    buffer = StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.CUSTOMREQUEST, verb)
-    c.setopt(c.HTTPHEADER, [authhead])
-    for arg in args:
-        c.setopt(c.POSTFIELDS, arg)
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    c.close()
-    return buffer.getvalue()
-
-def ns_get(fqdn):
-    out = check_output(['dig', '+short', fqdn, 'NS'])
-    legacy_ns = out.split()
-    return legacy_ns[0]
-
-def record_list(zone_json):
-    zone = json.loads(zone_json)
-    rec_list = []
-    for records in zone['records']:
-        if records['type'] != 'NS':
-            recs = {
-                'domain': records['domain'],
-                'type': records['type']
-            }
-            rec_list.append(recs)
-    return rec_list
-
-def lookup(record, type, ns):
-    out = check_output(['dig', '+nocmd', '+noall', '+answer', '+ttlid', type, '@' + ns, record])
-    answers = out.split("\n")
-    results = []
-    answers_list = []
-    for answer in answers:
-        if len(answer) != 0:
-            component = ' '.join(answer.split("\t"))
-            answers_list.append(component)
-            sorted(answers_list)
-        #sorted(results, key=itemgetter(len(results)-1))
-        results.append(answers_list)
-    return results
-
-def diff_rec(list_o_records):
-    old_list = []
-    new_list = []
-    diff_list = []
-    for record in list_o_records:
-        if re.match(r"^\*", record['domain']):
-            re.sub(r"\*", RANDOM_HOSTNAME, record['domain'], max=1)
-        old_list.append(lookup(record['domain'], record['type'], legacy_ns))
-        new_list.append(lookup(record['domain'], record['type'], NSONE_NS))
-    diff_list = filter(lambda x:x not in new_list, old_list)
-    return diff_list
-
-print('Please enter API key:')
-api_key = raw_input() #sanitize
-print('Please enter fully qualified domain name:') #sanitize
-fqdn = raw_input() #sanitize
-legacy_ns = ns_get(fqdn)
-list = record_list(curl_api(API_URI + "zones/" + fqdn, "GET", AUTH_HEAD + api_key))
-for poop in list:
-    pprint(lookup(poop['domain'], poop['type'], NSONE_NS))
 
