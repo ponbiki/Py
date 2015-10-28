1bb4af48d12c6996864c76a8a1b32ee969572093
ponbiki <jcandroscavage@hotmail.com>
2015-10-28 19:19:45 -0400

    gui test

---
+++ scripts/src/tinktest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+    tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
