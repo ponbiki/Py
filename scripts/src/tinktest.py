91417310bd6eb1bf55a64410f9bfcfa99c401a4b
ponbiki <jcandroscavage@hotmail.com>
2015-10-28 19:27:34 -0400

    gui test

--- scripts/src/tinktest.py
+++ scripts/src/tinktest.py
@@ -32,4 +32,10 @@ C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
 C1.pack()
 C2.pack()
 
+L1 = Label(top, text="User Name")
+L1.pack( side = LEFT)
+E1 = Entry(top, bd =5)
+
+E1.pack(side = RIGHT)
+
 top.mainloop()
