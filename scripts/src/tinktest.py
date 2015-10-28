032c4f64002d3e8f6776330d96f43f147230fc86
ponbiki <jcandroscavage@hotmail.com>
2015-10-28 19:26:15 -0400

    gui test

--- scripts/src/tinktest.py
+++ scripts/src/tinktest.py
@@ -2,6 +2,7 @@
 
 import Tkinter
 import tkMessageBox
+from Tkinter import *
 
 top = Tkinter.Tk()
 
@@ -11,4 +12,24 @@ def helloCallBack():
 B = Tkinter.Button(top, text = "Hello", command = helloCallBack)
 
 B.pack()
+
+
+C = Tkinter.Canvas(top, bg="blue", height=250, width=300)
+
+coord = 10, 50, 240, 210
+arc = C.create_arc(coord, start=0, extent=150, fill="red")
+
+C.pack()
+
+CheckVar1 = IntVar()
+CheckVar2 = IntVar()
+C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+C1.pack()
+C2.pack()
+
 top.mainloop()
