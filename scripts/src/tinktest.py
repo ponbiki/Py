2d1fafd42e7969d70b636a0d5c6454e12c61203d
ponbiki <jcandroscavage@hotmail.com>
2015-10-29 10:18:19 -0400

    gui testing still

--- scripts/src/tinktest.py
+++ scripts/src/tinktest.py
@@ -38,4 +38,28 @@ E1 = Entry(top, bd =5)
 
 E1.pack(side = RIGHT)
 
+var = StringVar()
+label = Label( top, textvariable=var, relief=RAISED )
+
+var.set("Hey!? How are you doing?")
+label.pack()
+
+frame = Frame(top)
+frame.pack()
+
+bottomframe = Frame(top)
+bottomframe.pack( side = BOTTOM )
+
+redbutton = Button(frame, text="Red", fg="red")
+redbutton.pack( side = LEFT)
+
+greenbutton = Button(frame, text="Brown", fg="brown")
+greenbutton.pack( side = LEFT )
+
+bluebutton = Button(frame, text="Blue", fg="blue")
+bluebutton.pack( side = LEFT )
+
+blackbutton = Button(bottomframe, text="Black", fg="black")
+blackbutton.pack( side = BOTTOM)
+
 top.mainloop()
