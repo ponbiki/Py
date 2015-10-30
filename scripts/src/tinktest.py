0debac01b45ec514302ab68d9524748e4bd62307
ponbiki <jcandroscavage@hotmail.com>
2015-10-30 10:35:00 -0400

    whitespace cleanup

--- scripts/src/tinktest.py
+++ scripts/src/tinktest.py
@@ -23,12 +23,8 @@ C.pack()
 
 CheckVar1 = IntVar()
 CheckVar2 = IntVar()
-C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
-C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
+C1 = Checkbutton(top, text = "Music", variable = CheckVar1, onvalue = 1, offvalue = 0, height=5, width = 20)
+C2 = Checkbutton(top, text = "Video", variable = CheckVar2, onvalue = 1, offvalue = 0, height=5, width = 20)
 C1.pack()
 C2.pack()
 
