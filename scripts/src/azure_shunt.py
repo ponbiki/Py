4181b63865d691d439f09a599f19ea61830e88fa
ponbiki <jcandroscavage@hotmail.com>
2015-11-17 15:38:49 -0500

    completed simple json shunt creator

--- scripts/src/azure_shunt.py
+++ scripts/src/azure_shunt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
+import json
 import xmltodict
-from pprint import pprint
 
-with open('PublicIPs_20151109.xml') as fd:
+with open('PublicIPs_20151109.xml') as fd: # http://www.microsoft.com/en-us/download/confirmation.aspx?id=41653
    obj = xmltodict.parse(fd.read())
 
 holder = []
@@ -15,4 +15,21 @@ for range in obj['AzurePublicIpAddresses']['Region']:
    for ip in range['IpRange']:
       subnet = ip['@Subnet']
       holder[x-1][name].append(subnet)
-pprint(holder)
+
+with open('azure_region.json') as az: # referenced https://azure.microsoft.com/en-us/regions/
+   template = json.loads(az.read())
+
+ip_list = []
+for region in holder:
+   ip_region = region.keys()
+   for reg in ip_region:
+      for ip in region[reg]:
+         ip_list.append({ip: ip_region[0]})
+
+shunt_list = []
+for sub in ip_list:
+   snkey = sub.keys()[0]
+   data = sub.values()[0]
+   shunt = template[data]
+   shuntjs = {snkey: shunt}
+   print(json.dumps(shuntjs))
