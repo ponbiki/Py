617b683aaa528e60c7a04e33cbdd38ba17dc872e
ponbiki <ponbiki@beryllium>
2015-11-22 00:00:54 -0500

    still more pasing add liveness test to finish parse

--- scripts/src/akaxml.py
+++ scripts/src/akaxml.py
@@ -21,7 +21,7 @@ class Ripper():
         for dc in obj['domain']['datacenter']:
             dc_dict = {"nickname": dc['nickname'],
                        "editable": dc['editable'],
-                       "score_penalty": ['score-penalty'],
+                       "score_penalty": dc['score-penalty'],
                        "city": dc['city'],
                        "state": dc['state-or-province'],
                        "country": dc['country'],
@@ -30,8 +30,24 @@ class Ripper():
             self.data_centers.append({dc['@name']: dc_dict})
         self.properties = []
         for prop in obj['domain']['property']:
+            ass_list = []
+            for ass in prop['assignment']:
+                ass_name = ass['@name']
+                ass_dict = {"status": ass['status'],
+                            "traffic_weight": ass['traffic-weight'],
+                            "servers": ass['servers']} # may require another for (does this take multiples?)
+                ass_list.append({ass_name: ass_dict})
             prop_dict = {"comments": prop['comments'],
-                         "map_type": prop['map.type']}
+                         "map_type": prop['map.type'],
+                         "ttl": prop['ttl'],
+                         "cname": prop['cname'], # probably will need a key grab on this to get type
+                         "failover_delay": prop['failover-delay'],
+                         "failback_delay": prop['failback-delay'],
+                         "persistent_assignment": prop['persistent-assignment'],
+                         "load_imbalance_factor": prop['load-imbalance-factor'],
+                         "dca_threshold_percent": prop['dca-threshold-percent'],
+                         "dca_threshold_constant": prop['dca-threshold-constant'],
+                         "assignments": ass_list}
             self.properties.append({prop['@name']: prop_dict})
 
 
