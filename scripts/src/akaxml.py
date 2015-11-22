f4498df98e59958e4de033aae22e320e1a1df6e3
ponbiki <ponbiki@beryllium>
2015-11-21 19:53:30 -0500

    more parsing of xml to dict items

--- scripts/src/akaxml.py
+++ scripts/src/akaxml.py
@@ -20,6 +20,8 @@ class Ripper():
         self.data_centers = []
         for dc in obj['domain']['datacenter']:
             dc_dict = {"nickname": dc['nickname'],
+                       "editable": dc['editable'],
+                       "score_penalty": ['score-penalty'],
                        "city": dc['city'],
                        "state": dc['state-or-province'],
                        "country": dc['country'],
@@ -28,10 +30,11 @@ class Ripper():
             self.data_centers.append({dc['@name']: dc_dict})
         self.properties = []
         for prop in obj['domain']['property']:
-            prop_dict = {}
+            prop_dict = {"comments": prop['comments'],
+                         "map_type": prop['map.type']}
             self.properties.append({prop['@name']: prop_dict})
 
 
 tester = Ripper('e.xx.openx.com.akadns.net.xsd')
 
-pprint(tester.data_centers)
+pprint(tester.properties)
