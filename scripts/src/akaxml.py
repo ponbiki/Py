6fddc5334e6f29558f7a5b6917a8820ebf744ae5
jandroscavage <jandroscavage@nsone.net>
2015-11-23 17:12:37 -0500

    Last of XML info parsed to dicts

--- scripts/src/akaxml.py
+++ scripts/src/akaxml.py
@@ -35,21 +35,36 @@ class Ripper():
                 ass_name = ass['@name']
                 ass_dict = {"status": ass['status'],
                             "traffic_weight": ass['traffic-weight'],
-                            "servers": ass['servers']} # may require another for (does this take multiples?)
+                            "servers": ass['servers']} # may require another for loop(does this take multiples?)
                 ass_list.append({ass_name: ass_dict})
+            if 'liveness-test' in prop:
+                test_type = prop['liveness-test']['@name']
+                test_dict = {"test_type": test_type,
+                             "test_interval": prop['liveness-test']['test.interval'],
+                             "test_timeout": prop['liveness-test']['test.timeout'],
+                             "test_protocol": prop['liveness-test']['test.protocol'],
+                             "test_port": prop['liveness-test']['test.port'],
+                             "test_object": prop['liveness-test']['test.object'],
+                             "test_suppress_warn": prop['liveness-test']['test.suppress-non-standard-port-warnings'],
+                             "test_password": prop['liveness-test']['test.password'],
+                             "test_username": prop['liveness-test']['test.username'],
+                             "test_host_header": prop['liveness-test']['test.http.use-host-header'],
+                             "test_fail_code": prop['liveness-test']['test.http.failures-codes']}
+            else:
+                test_dict = {"test_type": "none"}
             prop_dict = {"comments": prop['comments'],
                          "map_type": prop['map.type'],
                          "ttl": prop['ttl'],
-                         "cname": prop['cname'], # probably will need a key grab on this to get type
+                         "cname": prop['cname'], # probably will need a key grab on this to get type, but only one example to go on
                          "failover_delay": prop['failover-delay'],
                          "failback_delay": prop['failback-delay'],
                          "persistent_assignment": prop['persistent-assignment'],
                          "load_imbalance_factor": prop['load-imbalance-factor'],
                          "dca_threshold_percent": prop['dca-threshold-percent'],
                          "dca_threshold_constant": prop['dca-threshold-constant'],
-                         "assignments": ass_list}
+                         "assignments": ass_list,
+                         "test": test_dict}
             self.properties.append({prop['@name']: prop_dict})
-
 
 tester = Ripper('e.xx.openx.com.akadns.net.xsd')
 
---
+++ scripts/src/setup.py
@@ -0,0 +1,27 @@
+__author__ = "ponbiki"
+__date__ = "$Nov 23, 2015 10:58:03 AM$"
+
+from setuptools import setup, find_packages
+
+setup (
+       name='scripts',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='ponbiki',
+       author_email='',
+
+       summary='Just another Python package for the cheese shop',
+       url='',
+       license='',
+       long_description='Long description of the package',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
