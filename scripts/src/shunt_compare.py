0acf7361db31f7a3f7e0bc2f5333a65e56ed3d54
jandroscavage <jandroscavage@nsone.net>
2015-11-25 14:51:32 -0500

    new shunt to production shunt duplication check

---
+++ scripts/src/shunt_compare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from pprint import pprint
+
+new_shunt_file = 'azure.json'
+production_shunt_file = 'geo_shunts.json'
+
+new_list = []
+with open(new_shunt_file, 'r') as f:
+   for line in f:
+      new_list.append(line.split(None, 1)[0])
+
+comp_list = []
+with open(production_shunt_file, 'r') as g:
+   for lines in g:
+      if len(lines) > 1:
+         comp_list.append(lines.split(None, 1)[0])
+
+exist_list = []
+for ip in new_list:
+   for inpr in comp_list:
+      if ip == inpr:
+         exist_list.append(ip)
+
+pprint(exist_list)
