b58a1a8c3b23bab383857af34b3f0d4bd8f61126
ponbiki <ponbiki@beryllium>
2015-12-29 15:49:38 -0500

    simple backup script

---
+++ scripts/simple_back.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import json
+import pycurl
+import sys
+from StringIO import StringIO
+
+AUTH = 'X-NSONE-Key: ' + sys.argv[1]
+URL = 'https://api.nsone.net/v1/zones'
+
+def curl_api(url, verb, authhead, *args):
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.CUSTOMREQUEST, verb)
+    c.setopt(c.HTTPHEADER, [authhead])
+    for arg in args:
+        c.setopt(c.POSTFIELDS, arg)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    return buffer.getvalue()
+
+jsons = json.loads(curl_api(URL, 'GET', AUTH))
+
+zone_list = []
+
+for jss in jsons:
+   zone_list.append(jss['zone'])
+
+big_dic = {}
+
+for zone in zone_list:
+   record = curl_api(URL + '/' + zone, 'GET', AUTH)
+   thing = {zone: [json.loads(record)]}
+   big_dic.update(thing)
+
+print json.dumps(big_dic)
