87c7e74443eb86008fda0628037134fdcbb408ae
ponbiki <jcandroscavage@hotmail.com>
2016-03-06 23:04:19 -0500

    renamed class for pip compliance

--- scripts/src/mongo.py
+++ scripts/src/mongo.py
@@ -7,30 +7,31 @@ from pymongo import MongoClient
 database = 'test'
 collection = 'xxxx'
 
-class mongoShunt:
+
+class MongoShunt:
 
     def __init__(self):
         client = MongoClient()
         db = client.database
-        mongoShunt.gs = db.collection
+        MongoShunt.gs = db.collection
 
-    def findIp4(self, ip_addr):
-        mongoShunt.results = []
-        for shunt in mongoShunt.gs.find(ip_addr):
-            mongoShunt.results.append(shunt)
+    def find_ip4(self, ip_addr):
+        MongoShunt.results = []
+        for shunt in MongoShunt.gs.find(ip_addr):
+            MongoShunt.results.append(shunt)
 
     def search(self, ver):
         if ver == "4":
-            mongoShunt.ver = "ipv4"
+            MongoShunt.ver = "ipv4"
         elif ver == "6":
-            mongoShunt.ver = "ipv6"
+            MongoShunt.ver = "ipv6"
 
 def main():
-    x = mongoShunt()
+    x = MongoShunt()
     srch = sys.argv[1]
     rgx = r'.*' + re.escape(srch) + '.*'
     reg = re.compile(rgx)
-    x.findIp4({'prefixes':reg})
+    x.find_ip4({'prefixes':reg})
 
 
 if __name__ == '__main__':
