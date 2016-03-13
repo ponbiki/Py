1e1d25d54b9dbf858cab8869631fbc9c634fa3a1
ponbiki <jcandroscavage@hotmail.com>
2016-03-13 01:02:13 -0500

    added iterator

--- scripts/src/giggle.py
+++ scripts/src/giggle.py
@@ -3,7 +3,7 @@
 import curses
 
 
-def get_param(prompt_string):
+def get_param(screen, prompt_string):
     screen.clear()
     screen.border(0)
     screen.addstr(2, 2, prompt_string)
@@ -11,53 +11,64 @@ def get_param(prompt_string):
     x_input = screen.getstr(4, 4, 20)
     return x_input
 
-x = 0
-x2 = 0
 
+def main(screen):
 
-while x != ord('0'):
-    screen = curses.initscr()
-    curses.start_color()
+    x = 0
+    x2 = 0
 
-    screen.clear()
-    screen.border(0)
-    screen.addstr(2, 2, "Geo Shunt Tool -- Please select an action")
-    screen.addstr(4, 4, "1 - Search for an existing shunt")
-    screen.addstr(5, 4, "2 - Add a new shunt")
-    screen.addstr(6, 4, "3 - Edit an existing shunt")
-    screen.addstr(7, 4, "4 - Delete an existing shunt")
-    screen.addstr(9, 4, "0 - Exit")
-    screen.refresh()
+    while x != ord('0'):
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLUE)
+        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
-    x = screen.getch()
-
-    if x == ord('1'):
-        ip_addr = get_param("Enter partial or full IP address or CIDR")
+        screen.bkgd(curses.color_pair(1))
         screen.refresh()
 
-    if x == ord('2'):
-        while x2 != ord('0'):
-            screen.clear()
-            screen.border(0)
-            screen.addstr(2, 2, "Shunt Protocol")
-            screen.addstr(4, 4, "1 - IPv4")
-            screen.addstr(5, 4, "2 - IPv6")
-            screen.addstr(7, 4, "0 - Go Back")
-            screen.refresh
+        screen.border(0)
+        screen.addstr(2, 2, "Geo Shunt Tool -- Please select an action")
+        screen.addstr(4, 4, "1 - Search for an existing shunt")
+        screen.addstr(5, 4, "2 - Add a new shunt")
+        screen.addstr(6, 4, "3 - Edit an existing shunt")
+        screen.addstr(7, 4, "4 - Delete an existing shunt")
+        screen.addstr(9, 4, "0 - Exit")
+        screen.refresh()
 
-            x2 = screen.getch()
+        x = screen.getch()
 
-            if x2 == ord('1'):
+        if x == ord('1'):
+            ip_addr = get_param("Enter partial or full IP address or CIDR")
+            screen.refresh()
+
+        if x == ord('2'):
+            while x2 != ord('0'):
                 screen.clear()
                 screen.border(0)
-                city_name = get_param("City Name")
-                state_name = get_param("State/Province/Territory Name")
-                country_name = get_param("Country Name")
-                time_zone = get_param("Time Zone")
+                screen.addstr(2, 2, "Shunt Protocol")
+                screen.addstr(4, 4, "1 - IPv4")
+                screen.addstr(5, 4, "2 - IPv6")
+                screen.addstr(7, 4, "0 - Go Back")
                 screen.refresh()
 
-            if x2 == ord('0'):
-                curses.endwin()
+                x2 = screen.getch()
 
-    if x == ord('0'):
-        curses.endwin()
+                if x2 == ord('1'):
+                    screen.clear()
+                    screen.border(0)
+                    city_name = get_param("City Name")
+                    state_name = get_param("State/Province/Territory Name")
+                    country_name = get_param("Country Name")
+                    time_zone = get_param("Time Zone")
+                    screen.refresh()
+
+                if x2 == ord('0'):
+                    curses.endwin()
+
+        if x == ord('0'):
+            curses.endwin()
+
+try:
+    curses.wrapper(main)
+except KeyboardInterrupt:
+    print "User requested exti!"
+    exit()
