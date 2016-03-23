d2079fb0b130a247562d999591e703bfe082f39c
ponbiki <jcandroscavage@hotmail.com>
2016-03-22 23:59:24 -0400

    widened pad

---
+++ scripts/root.py
@@ -0,0 +1,11 @@
+#!/usr/local/env python
+
+guess = 5
+print "I want to find the square root of:"
+answer = raw_input()
+float(answer)
+print "Guess - %i" % (guess)
+for i in range(40):
+    if float(guess) * float(guess) != float(answer):
+        guess = (float(guess) + (float(answer) / float(guess)))/2
+        print guess
