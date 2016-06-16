59e8e2938f3d767649df199d21868675b67bd5e2
ponbiki <jcandroscavage@hotmail.com>
2016-06-16 16:39:43 -0400

    full information to stderr in the event bgp is not in state ESTABLISHED minor conf tweak deletion of superfluous code

--- scripts/bgp_status.py
+++ scripts/bgp_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # bgp_status.py -- a collector for tcollector/OpenTSDB
-# Copyright (C) 2015 NSONE, Inc.
+# Copyright (C) 2016 NSONE, Inc.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
@@ -13,7 +13,7 @@
 # of the GNU Lesser General Public License along with this program.  If not,
 # see <http://www.gnu.org/licenses/>.
 
-'''BGP Up Down Status Collector'''
+''' BGP Up Down Status Collector'''
 
 import sys
 import time
@@ -22,50 +22,59 @@ import psutil
 
 INTERVAL = 30
 BGP_CONF = '/etc/nsone/bgp.conf'
+BGP_CONF_V6 = '/etc/nsone/bgp.conf-v6'
 BGP_UP = 'bgp-up.conf'
+BGP_UP_v6 = 'bgp-up.conf-v6'
 PUERTO = 179
 
 
 def check_BGP():
     '''
-    Checks bgp.conf for up / down, and the sockets for v4 and v6 BGP established.
+    Checks bgp.conf for up / down, and the sockets for IPv4 BGP established.
     Returns in the format of:
-    bgp.status <time> <0|1> protocol=<IPv4|IPv6>
+    bgp.status <time> <0|1> protocol=IPv4
     '''
     thyme = int(time.time())
-    v6_up = 0
     v4_up = 0
-    proc_names = {}
     if os.readlink(BGP_CONF) != BGP_UP:
-        print 'bgp.status %d %d protocol=IPv6' % (thyme, v6_up)
         print 'bgp.status %d %d protocol=IPv4' % (thyme, v4_up)
     else:
-        for p in psutil.process_iter():
-            try:
-                proc_names[p.pid] = p.name()
-            except psutil.Error:
-                pass
         for c in psutil.net_connections(kind='inet'):
             if c.raddr:
                 if c.raddr[1] == PUERTO:
                     if c.status == 'ESTABLISHED':
-                        if ':' in c.raddr[0]:
+                        v4_up = 1
+                    else:
+                        print >> sys.stderr, 'IPv4 not ESTABLISHED: ', c
+        print 'bgp.status %d %d protocol=IPv4' % (thyme, v4_up)
+
+
+def check_BGP6():
+    '''
+    Checks bgp.conf for up / down, and the sockets for IPv6 BGP established.
+    Returns in the format of:
+    bgp.status <time> <0|1> protocol=IPv6
+    '''
+    thyme = int(time.time())
+    v6_up = 0
+    if os.readlink(BGP_CONF_V6) != BGP_UP_v6:
+        print 'bgp.status %d %d protocol=IPv6' % (thyme, v6_up)
+    else:
+        for c in psutil.net_connections(kind='inet'):
+            if c.raddr:
+                if c.raddr[1] == PUERTO:
+                    if ':' in c.raddr[0]:
+                        if c.status == 'ESTABLISHED':
                             v6_up = 1
                         else:
-                            v4_up = 1
-                    else:
-                        continue
-                else:
-                    continue
-            else:
-                continue
+                            print >> sys.stderr, 'IPv6 not ESTABLISHED: ', c
         print 'bgp.status %d %d protocol=IPv6' % (thyme, v6_up)
-        print 'bgp.status %d %d protocol=IPv4' % (thyme, v4_up)
 
 
 def main():
     while True:
         check_BGP()
+        check_BGP6()
         sys.stdout.flush()
         if not INTERVAL or INTERVAL < 1:
             break
