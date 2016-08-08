0cb3916ac28f12c15d4acb363eb5868a437b5699
ponbiki <jcandroscavage@hotmail.com>
2016-08-08 11:10:59 -0400

    extra reporting and validation

--- scripts/orphan_feed.py
+++ scripts/orphan_feed.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 
 holder = {}
 
-with open("/home/ponbiki/Documents/client_2016.txt") as fl:
+with open("client_2016.txt") as fl:
     for line in fl:
         x = line.rstrip()
         y = x.split()
@@ -18,24 +18,30 @@ with open("/home/ponbiki/Documents/client_2016.txt") as fl:
 
 client = MongoClient('mongodb://***:***@localhost:27017')
 db = client['***']
-collection = db.d_feeds
+collection = db.df
 
 for key in holder:
     try:
         doc = collection.find_one({"_id": ObjectId(key)})
-    except:
-        pass
+    except Exception as e:
+        print "Couldn't load document for %s:\n %s" % (key, e)
+        continue
     new_dest = []
     removed = []
     original = 0
     deleted = 0
     for dest in doc['destinations']:
         original += 1
-        if str(dest['record']) not in holder[key]:
+        if (str(dest['record']) not in holder[key]) or (dest['desttype'] != "answer"):
+            print "data_feed %s Saving:" % key
+            pprint(dest)
             new_dest.append(dest)
         else:
+            print "data_feed %s Deleting:" % key
+            pprint(dest)
             deleted += 1
             removed.append(dest)
+    pprint(new_dest)
     del doc['destinations']
     doc['destinations'] = new_dest
     collection.save(doc)
@@ -44,6 +50,3 @@ for key in holder:
     print "The following %d destinations were removed from %s, leaving %d destinations\n" % (deleted, key, original-deleted)
     for rem in removed:
         pprint(rem)
-
-
-# cat <file> |cut -d" " -f 4-5| awk '{ print $1 " "$2 }' | cut -c6-29,31-31,36-
