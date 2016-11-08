20f60dc823ebb3d71dcf808bfaf56100462bb8e9
ponbiki <jcandroscavage@hotmail.com>
2016-11-08 13:40:18 -0500

    variable renaming and functionality separation for clarity

--- scripts/priv_rev_zone.py
+++ scripts/priv_rev_zone.py
@@ -2,28 +2,29 @@ import ipaddress
 
 
 def priv_rev(zone):
-    ipv4 = '.in-addr.arpa'
-    ipv6 = '.ip6.arpa'
-    if zone.rstrip('.').lower().endswith(ipv4):
-        addr_list = [i for i in reversed(zone[:-len(ipv4)].split('.'))]
+    _ipv4 = '.in-addr.arpa'
+    _ipv6 = '.ip6.arpa'
+    if zone.rstrip('.').lower().endswith(_ipv4):
+        addr_list = [i for i in reversed(zone[:-len(_ipv4)].split('.'))]
         while len(addr_list) < 4:
             addr_list.append('0')
+        fwd_addr = unicode('.'.join(addr_list).split('/')[0])
         try:
-            return ipaddress.ip_address(unicode('.'.join(addr_list).split('/')[0])).is_private
-        except:
+            return ipaddress.ip_address(fwd_addr).is_private
+        except ValueError:
             return False
-    elif zone.rstrip('.').lower().endswith(ipv6):
-        flipped_rev6 = ''.join(reversed(zone[:-len(ipv6)].split('.')))
-        flipped_rev6 = [flipped_rev6[i:i + 4] for i in xrange(0, len(flipped_rev6), 4)]
-        new_flipped_rev6 = []
-        for j in flipped_rev6:
+    elif zone.rstrip('.').lower().endswith(_ipv6):
+        flipped_rev6 = ''.join(reversed(zone[:-len(_ipv6)].split('.')))
+        rev6_list = [flipped_rev6[i:i + 4] for i in xrange(0, len(flipped_rev6), 4)]
+        new_rev6_list = []
+        for j in rev6_list:
             while len(j) < 4:
                 j += str(0)
-            new_flipped_rev6.append(j)
-        if len(new_flipped_rev6) < 8:
-            new_flipped_rev6.append(':')
-        ipv6_final = unicode(':'.join(new_flipped_rev6))
+            new_rev6_list.append(j)
+        if len(new_rev6_list) < 8:
+            new_rev6_list.append(':')
+        ipv6_final = unicode(':'.join(new_rev6_list))
         try:
             return ipaddress.ip_address(ipv6_final).is_private
-        except:
+        except ValueError:
             return False
