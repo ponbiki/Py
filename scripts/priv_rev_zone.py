500f63fc4fee8895f4be4159e9e5fec401b8a33a
ponbiki <jcandroscavage@hotmail.com>
2016-11-09 15:35:54 -0500

    final form (maybe)

--- scripts/priv_rev_zone.py
+++ scripts/priv_rev_zone.py
@@ -1,20 +1,22 @@
-import ipaddress
+from ipaddress import ip_address
 
 
-def is_priv_rev(zone):
+@staticmethod
+def is_priv_revdns(input):
     _ipv4 = '.in-addr.arpa'
     _ipv6 = '.ip6.arpa'
-    if zone.rstrip('.').lower().endswith(_ipv4):
-        addr_list = [i for i in reversed(zone[:-len(_ipv4)].split('.'))]
+    _fqdn = input.rstrip('.').lower()
+    if _fqdn.endswith(_ipv4):
+        addr_list = [i for i in reversed(_fqdn[:-len(_ipv4)].split('.'))]
         while len(addr_list) < 4:
             addr_list.append('0')
         fwd_addr = unicode('.'.join(addr_list).split('/')[0])
         try:
-            return ipaddress.ip_address(fwd_addr).is_private
+            return ip_address(fwd_addr).is_private
         except ValueError:
             return False
-    elif zone.rstrip('.').lower().endswith(_ipv6):
-        flipped_rev6 = ''.join(reversed(zone[:-len(_ipv6)].split('.')))
+    elif _fqdn.endswith(_ipv6):
+        flipped_rev6 = ''.join(reversed(_fqdn[:-len(_ipv6)].split('.')))
         rev6_list = [flipped_rev6[i:i + 4] for i in xrange(0, len(flipped_rev6), 4)]
         new_rev6_list = []
         for j in rev6_list:
@@ -25,7 +27,7 @@ def is_priv_rev(zone):
             new_rev6_list.append(':')
         ipv6_final = unicode(':'.join(new_rev6_list))
         try:
-            return ipaddress.ip_address(ipv6_final).is_private
+            return ip_address(ipv6_final).is_private
         except ValueError:
             return False
     else:
